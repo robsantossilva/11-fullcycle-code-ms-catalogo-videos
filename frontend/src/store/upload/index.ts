2d257b908c5f249eb97be8e462b73fbe8014de0f
Robson Silva <robsantossilva@gmail.com>
2022-02-24 17:57:54 -0300

    Criando reducer para adicionar arquivo na fila de upload

--- frontend/src/store/upload/index.ts
+++ frontend/src/store/upload/index.ts
@@ -1,6 +1,6 @@
 import * as Typings from "./types";
 import {createActions, createReducer} from 'reduxsauce';
-import { AddUploadAction } from "./types";
+import update from 'immutability-helper';
 
 // Criando Action Types e Creators
 export const {Types, Creators} = createActions<{
@@ -30,7 +30,39 @@ const reducer = createReducer<Typings.State, Typings.Actions>(INITIAL_STATE, {
 export default reducer;
 
 function addUpload(state = INITIAL_STATE, action: Typings.AddUploadAction): Typings.State {
+    if(!action.payload.files.length){
+        return state;
+    }
+    
+    //Retorna o index do upload caso já exista
+    const index = findIndexUpload(state, action.payload.video.id);
+
+    if(index!==-1 && state.uploads[index].progress < 1){
+        return state;
+    }
+
+    const uploads = (index === -1) // Se o upload não existe 
+        ? state.uploads // mantem a mesma lista de uploads
+        : update(state.uploads, { // se não remove o que já existe para adiciona-lo novamente com novos files
+            $splice: [[index, 1]]
+        });
+
     return {
-        uploads: []
+        uploads: [
+            ...uploads,
+            {
+                video: action.payload.video,
+                progress: 0,
+                files: action.payload.files.map(file =>({
+                   fileField: file.fileField,
+                   filename: file.file.name,
+                   progress: 0
+                }))
+            }
+        ]
     };
+}
+
+function findIndexUpload(state: Typings.State, id: string){
+    return state.uploads.findIndex((upload: Typings.Upload) => upload.video.id === id);
 }
